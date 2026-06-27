bb1d02c94cc3597178848e8ae161ec436e4149fe
Pragnya-15 <pragnyadidugu3@gmail.com>
2026-06-27 18:20:53 +0530

    update api url

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/login", {
+      const res = await fetch("https://shopez-klpr.onrender.com/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -24,7 +24,7 @@ export default function Register() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/register", {
+      const res = await fetch("https://shopez-klpr.onrender.com/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: form.name, email: form.email, password: form.password }),
